843554eab5734b56de58b9528288098d48e44f7c
Alexandr Borisov <sasha@ubuntuSasha>
2015-08-05 15:38:08 +0300

    змінив формування сторінки about вивод робиться через шаблон а не в файлі view додав аплікейшни та шлях для пошуку шаблонів в settings.py змінив формування сторінки startpage вивод робиться через шаблон а не в файлі view, але шаблон знаходиться не в папці аплікейшна а в корні проекту

--- about/views.py
+++ about/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the aboutpage.")
+    output = "Hello, world. You're at the aboutpage."
+    template = loader.get_template('about/index.html')
+    return HttpResponse(template.render(output))
 
--- startpage/views.py
+++ startpage/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the startpage.")
+    output = "Hello, world. You're at the startpage."
+    template = loader.get_template('startpage/index.html')
+    return HttpResponse(template.render(output))
